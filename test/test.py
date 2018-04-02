5d81bbde3ac1a4b01a7589c055a18b1127380d2e
Parth Verma <v.parth98@gmail.com>
2018-04-02 21:11:34 +0530

    Added tests to check for existence of certain files
    Resolves #37 which required to create a test which should check for the
    existence of certain files.

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 import unittest
 import datetime
+import os
 
 class TestLicenseYear(unittest.TestCase):
 
@@ -14,6 +15,36 @@ class TestLicenseYear(unittest.TestCase):
                     copyright_line = line.strip()
                     break
         self.assertEqual('Copyright (c) 2016-%s SendGrid, Inc.' % datetime.datetime.now().year, copyright_line)
+
+class TestFileExistence(unittest.TestCase):
+    """
+    Add the list of files required to the files_list array.
+    For group of files out of which only one is required, add the file paths in an array.
+    """
+    def setUp(self):
+        self.files_list = [
+            ['./Docker','docker/Docker'],
+            ['./docker-compose.yml','./docker/docker-compose.yml'],
+            ['./.env_sample'],
+            ['./.gitignore'],
+            ['./.travis.yml'],
+            ['./.codeclimate.yml'],
+            ['./CHANGELOG.md'],
+            ['./CODE_OF_CONDUCT.md'],
+            ['./CONTRIBUTING.md'],
+            ['./.github/ISSUE_TEMPLATE'],
+            ['./LICENSE.md'],
+            ['./.github/PULL_REQUEST_TEMPLATE'],
+            ['./README.md'],
+            ['./TROUBLESHOOTING.md'],
+            ['./USAGE.md'],
+            ['./USE_CASES.md']
+            ]
+
+    def test_file_existence(self):
+        for file in self.files_list:
+            self.assertTrue(any(map(os.path.exists,file)),msg='%s does not exist.'%' or '.join(file))
+
 
 if __name__ == '__main__':
     unittest.main()
