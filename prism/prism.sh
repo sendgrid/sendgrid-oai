69b4cdc2e3c62444ae85eafa3d016cd4c6f5df45
Jennifer Mah <jennifermah98@gmail.com>
2021-06-14 18:43:31 -0700

    fix: change docker spec

--- prism/prism.sh
+++ prism/prism.sh
@@ -3,7 +3,6 @@ set -e
 
 rm -rf prism && mkdir -p prism && cd prism
 git clone --depth 1 https://github.com/sendgrid/sendgrid-oai .
-git checkout eb7a825bf06dfec7da2622735c5334c0d35da9fa
 cd prism
 
 docker-compose build --parallel
