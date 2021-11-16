3f6db5ec60566441f39e8bd1b9349f023b74eba6
Elise Shanholtz <eshanholtz@twilio.com>
2021-11-16 11:48:50 -0800

    revert

--- prism/prism.sh
+++ prism/prism.sh
@@ -8,7 +8,7 @@ cd prism
 docker-compose build --parallel
 
 if [ -z "$command" ]; then
-  docker-compose up --force-recreate
+  docker-compose up --force-recreate --abort-on-container-exit
 else
   docker-compose run helper-runner "$command"
   docker-compose down
